434e491db71abc3c3d08f1791c50ff989b25846d
Debian <admin@ip-172-31-39-238.us-east-2.compute.internal>
2020-05-05 16:56:15 +0000

    Add initial rails config.

---
+++ upload_to_futter/app/controllers/pages_controller.rb
@@ -0,0 +1,4 @@
+class PagesController < ApplicationController
+  def upload
+  end
+end
--- upload_to_futter/entrypoint.sh
+++ upload_to_futter/entrypoint.sh
@@ -1,1 +1,8 @@
-bundle exec rails server --environment production --binding 0.0.0.0 --port 3000
+#!/bin/bash
+set -e
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /upload_to_futter/tmp/pids/server.pid
+
+# Execute the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
