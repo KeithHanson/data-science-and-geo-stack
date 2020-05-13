ff9824faeff46c59ea7312feafd3a8ddd8e61d0d
Debian <admin@ip-172-31-39-238.us-east-2.compute.internal>
2020-05-13 22:42:19 +0000

    Update CSV upload form.

--- upload_to_futter/app/controllers/uploads_controller.rb
+++ upload_to_futter/app/controllers/uploads_controller.rb
@@ -1,3 +1,5 @@
+require 'shellwords'
+
 class UploadsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
@@ -7,9 +9,26 @@ class UploadsController < ApplicationController
   def submit
     file_name = params["csv-file"].original_filename rescue nil
     file_path = params["csv-file"].tempfile.path rescue nil
+    table_name = file_name.gsub(/[^0-9A-Za-z]/, '_').gsub(/_csv/, '').downcase
 
     if file_name && file_path
-      `./pgfutter --dbname #{ENV['PGFUTTER_DATABASE']} --host #{ENV['PGFUTTER_HOST']} --port #{ENV['PGFUTTER_PORT']} --username #{ENV['PGFUTTER_USERNAME']} --pass #{ENV['PGFUTTER_PASSWORD']} --table #{file_name} csv #{file_path}`
+      # Drop the existing table if it exists.
+      `PGPASSWORD=#{ENV['PGFUTTER_PASSWORD']} psql \
+        --host #{ENV['PGFUTTER_HOST']} \
+        --port #{ENV['PGFUTTER_PORT']} \
+        --username #{ENV['PGFUTTER_USERNAME']} \
+        --dbname #{ENV['PGFUTTER_DATABASE']} \
+        -c 'DROP TABLE IF EXISTS import."#{table_name}";'`
+
+      # Create and import into a new table.
+      `./pgfutter \
+        --host #{ENV['PGFUTTER_HOST']} \
+        --port #{ENV['PGFUTTER_PORT']} \
+        --username #{ENV['PGFUTTER_USERNAME']} \
+        --pass #{ENV['PGFUTTER_PASSWORD']} \
+        --dbname #{ENV['PGFUTTER_DATABASE']} \
+        --table #{Shellwords.escape(table_name)} \
+        csv #{file_path}`
     end
   end
 end
